95d776457c57dfd8b48583b1e2b71169fd730ced
Kostiantyn Skrypnyk <skrypnyk.kostiantyn@gmail.com>
2022-10-25 18:02:57 +0000

    Add S3 bucket

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3-bucket" {
   source  = "app.terraform.io/nordcloud-kskrypnyk/s3-bucket/aws"
   version = "2.8.0"
-  bucket = "${prefix}-my-s3-bucket"
+  bucket = "${var.prefix}-my-s3-bucket"
 }
